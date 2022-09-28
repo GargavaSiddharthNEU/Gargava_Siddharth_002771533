5c0629f0946628db5e952537dc410d01363f070a
GargavaSiddharthNEU <gargava.s@northeastern.edu>
2022-09-28 13:20:32 -0400

    28th Sep 1:20pm: Added CRUD features

---
+++ HRManTest/src/model/EmployeeHistory.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author siddh
+ */
+public class EmployeeHistory {
+    
+    private ArrayList<ManagerVar> history;
+    
+    public EmployeeHistory(){
+        this.history = new ArrayList<ManagerVar>();
+    }
+
+    public ArrayList<ManagerVar> getHistory() {
+        return history;
+    }
+
+    public void setHistory(ArrayList<ManagerVar> history) {
+        this.history = history;
+    }
+    
+    public ManagerVar addEmployeeDetails(){
+        ManagerVar newEmployee = new ManagerVar();
+        history.add(newEmployee);
+        
+        return newEmployee;
+    }
+    
+    public void deleteEmployee(ManagerVar mv){
+        history.remove(mv);
+    }
+    
+}
---
+++ HRManTest/src/model/ManagerVar.java
@@ -0,0 +1,109 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author siddh
+ */
+public class ManagerVar {
+    private String name;
+    private String empID;
+    private int age;
+    private String gender;
+    private String position;
+    private String teamInfo;
+    private String startDate;
+    private String level;
+    private String cellNum;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmpID() {
+        return empID;
+    }
+
+    public void setEmpID(String empID) {
+        this.empID = empID;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getTeamInfo() {
+        return teamInfo;
+    }
+
+    public void setTeamInfo(String teamInfo) {
+        this.teamInfo = teamInfo;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getCellNum() {
+        return cellNum;
+    }
+
+    public void setCellNum(String cellNum) {
+        this.cellNum = cellNum;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    @Override
+    public String toString(){
+        return name;
+    }
+    
+    
+}
